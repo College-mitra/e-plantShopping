c9033fa6ca1e4ebd58d80ac28c560c7ee24cce19
College-mitra <b3050426@gmail.com>
2025-12-31 09:14:18 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import ProductList from "./components/ProductList";
+
+function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
+  return (
+    <div className="background-image">
+      {!showProductList ? (
+        <div>
+          <h1>Paradise Nursery</h1>
+          <button onClick={() => setShowProductList(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
+    </div>
+  );
+}
+
+export default App;
